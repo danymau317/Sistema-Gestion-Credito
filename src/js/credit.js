7af0600cf1d38b764c25c5f3e0143c67bb040594
Daniel Mauricio Saavedra Gonzalez <danymau317@gmail.com>
2025-04-14 23:33:53 -0600

    Inicio de Desarollo de Gestion de Credito

---
+++ src/js/credit.js
@@ -0,0 +1,11 @@
+export function listTab() {
+    let listItems = document.querySelectorAll('.list__item');
+
+    listItems.forEach((item) => {
+        item.addEventListener('click', () => {
+            listItems.forEach((el) => el.classList.remove('list__item--selected'));
+            item.classList.add('list__item--selected');
+        });
+    });
+
+}
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import { listTab } from "./credit.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    listTab();
+});
